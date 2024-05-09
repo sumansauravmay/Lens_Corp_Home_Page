059b97bdd0e56d8fe74418d2b79465094072b410
sumansauravmay <sumansauravmay12345@gmail.com>
2024-05-10 02:09:51 +0530

    Navbar

---
+++ lens_home_page/src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    Avatar,
+  } from '@chakra-ui/react';
+
+const Intro = () => {
+  return (
+    <div class="text-yellow-600">
+      Hi
+    </div>
+  )
+}
+
+export default Intro
---
+++ lens_home_page/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+
+import {
+  Box,Text,
+  Flex,
+  Avatar, 
+  Button,
+  useColorModeValue,
+  Stack,
+  useColorMode
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+
+export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <>
+      <Box bg={useColorModeValue('#FFFFFF')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-around'}>
+            <Stack>
+                <Avatar name='logo' src="https://lenscorp.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnav_logo.e5fb945a.png&w=96&q=75"/>
+            </Stack>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={15}>
+                <Box mt="5px">MakeMyWeb.</Box>
+                <Box mt="5px">Home</Box>
+                <Box mt="5px">Company</Box>
+                <Box mt="5px">Blogs</Box>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
