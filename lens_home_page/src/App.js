3581a81431311cac2661980cd23dfa7a29f539da
sumansauravmay <sumansauravmay12345@gmail.com>
2024-05-10 23:35:20 +0530

    services

--- lens_home_page/src/App.js
+++ lens_home_page/src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Footer from './components/Footer';
 import Intro from './components/Intro';
 import Navbar from './components/Navbar';
+import Services from './components/Services';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Navbar/>
     <Intro/>
     <About/>
+    <Services/>
     <br/>
     <Footer/>
     </div>
--- lens_home_page/src/components/Footer.jsx
+++ lens_home_page/src/components/Footer.jsx
@@ -1,112 +1,108 @@
 import {
-    Box,
-    chakra,
-    Container,
-    Link,
-    SimpleGrid,
-    Stack,
-    Text,
-    VisuallyHidden,
-    useColorModeValue,
-    Avatar,
-  } from '@chakra-ui/react';
-  import { FaInstagram, FaFacebook, FaLinkedin, FaDiscord } from 'react-icons/fa';
-  import { FaXTwitter } from "react-icons/fa6";
+  Box,
+  chakra,
+  Container,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  VisuallyHidden,
+  useColorModeValue,
+  Avatar,
+} from "@chakra-ui/react";
+import { FaInstagram, FaFacebook, FaLinkedin, FaDiscord } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
-  
-  const SocialButton = ({
-    children,
-    label,
-    href,
-  }) => {
-    return (
-      <chakra.button
-        bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-        rounded={'full'}
-        w={8}
-        h={8}
-        cursor={'pointer'}
-        as={'a'}
-        href={href}
-        display={'inline-flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-        transition={'background 0.3s ease'}
-        _hover={{
-          bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-        }}>
-        <VisuallyHidden>{label}</VisuallyHidden>
-        {children}
-      </chakra.button>
-    );
-  };
-  
-  const ListHeader = ( {children} ) => {
-    return (
-      <Text fontWeight={'550'} fontSize={'lg'} mb={2}>
-        {children}
-      </Text>
-    );
-  };
-  
-  export default function Footer() {
-    return (
-      <Box
-        bg={useColorModeValue('white')}
-        color={useColorModeValue('#000000')}>
-        <Container as={Stack} maxW={'6xl'} py={10}>
-          <SimpleGrid
-            templateColumns={{ sm: '1fr 1fr', md: '2fr 2fr 1fr' }}
-            spacing={8}>
-            <Stack spacing={6}>
-              <Box>
-              </Box>
-              <Avatar src="https://lenscorp.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnav_logo.e5fb945a.png&w=96&q=75"/>
-              <Text fontSize={'sm'}>
-              Tomorrow's Vision, Today!
-              </Text>
-              <Stack direction={'row'} spacing={3}>
+const SocialButton = ({ children, label, href }) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
+      rounded={"full"}
+      w={8}
+      h={8}
+      cursor={"pointer"}
+      as={"a"}
+      href={href}
+      display={"inline-flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      transition={"background 0.3s ease"}
+      _hover={{
+        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
 
-              <SocialButton label={'Facebook'} href={'https://www.facebook.com/'}>
-              <FaFacebook />
-                </SocialButton>
-                <SocialButton label={'Instagram'} href={'https://www.instagram.com/'}>
-                  <FaInstagram />
-                </SocialButton>
+const ListHeader = ({ children }) => {
+  return (
+    <Text fontWeight={"550"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
+};
 
-                <SocialButton label={'LinkedIn'} href={'https://www.linkedin.com/'}>
+export default function Footer() {
+  return (
+    <Box bg={useColorModeValue("white")} color={useColorModeValue("#000000")}>
+      <Container as={Stack} maxW={"6xl"} py={10}>
+        <SimpleGrid
+          templateColumns={{ sm: "1fr 1fr", md: "2fr 2fr 1fr" }}
+          spacing={8}
+        >
+          <Stack spacing={6}>
+            <Box></Box>
+            <Avatar src="https://lenscorp.ai/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnav_logo.e5fb945a.png&w=96&q=75" />
+            <Text fontSize={"sm"}>Tomorrow's Vision, Today!</Text>
+            <Stack direction={"row"} spacing={3}>
+              <SocialButton
+                label={"Facebook"}
+                href={"https://www.facebook.com/"}
+              >
+                <FaFacebook />
+              </SocialButton>
+              <SocialButton
+                label={"Instagram"}
+                href={"https://www.instagram.com/"}
+              >
+                <FaInstagram />
+              </SocialButton>
+
+              <SocialButton
+                label={"LinkedIn"}
+                href={"https://www.linkedin.com/"}
+              >
                 <FaLinkedin />
-                </SocialButton>
+              </SocialButton>
 
-                <SocialButton label={'Discord'} href={'https://discord.com/'}>
-                  <FaDiscord />
-                </SocialButton>
+              <SocialButton label={"Discord"} href={"https://discord.com/"}>
+                <FaDiscord />
+              </SocialButton>
 
-                <SocialButton label={'Twitter'} href={'https://twitter.com/'}>
+              <SocialButton label={"Twitter"} href={"https://twitter.com/"}>
                 <FaXTwitter />
-                </SocialButton>
-               
-              
-              </Stack>
+              </SocialButton>
             </Stack>
-            <Stack align={'flex-start'}>
-              <ListHeader>SITEMAP</ListHeader>
-              <Link href={'#'}>MakeMyWeb.</Link>
-              <Link href={'#'}>Services</Link>
-              <Link href={'#'}>Products</Link>
-              <Link href={'#'}>Blogs</Link>
-              <Link href={'#'}>Life at LENS</Link>
-            </Stack>
-            <Stack align={'flex-start'}>
-              <ListHeader>CONNECT</ListHeader>
-              <Text>+1-517-9300-792</Text>
-              <Text>+91-9990-736-796</Text>
-              <Text>solutions@lenscorp.ai</Text>
-              
-            </Stack>
-          </SimpleGrid>
-        </Container>
-      </Box>
-    );
-  }
-  
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>SITEMAP</ListHeader>
+            <Link href={"#"}>MakeMyWeb.</Link>
+            <Link href={"#"}>Services</Link>
+            <Link href={"#"}>Products</Link>
+            <Link href={"#"}>Blogs</Link>
+            <Link href={"#"}>Life at LENS</Link>
+          </Stack>
+          <Stack align={"flex-start"}>
+            <ListHeader>CONNECT</ListHeader>
+            <Text>+1-517-9300-792</Text>
+            <Text>+91-9990-736-796</Text>
+            <Text>solutions@lenscorp.ai</Text>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
+}
